3979eb4b5ee7c9acdac3c55387dc8aaa6d63f1fe
mkingmking <80473458+mkingmking@users.noreply.github.com>
2023-12-17 22:02:38 +0300

    login page

---
+++ lib/loginPage.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override // Corrected annotation with a lowercase 'o'
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                'arayuz/arayuz2.png'), // Make sure this path is correct
+            fit: BoxFit.cover, // This will fill the background of the Container
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize:
+                  MainAxisSize.min, // Use the minimum space for the column
+              mainAxisAlignment: MainAxisAlignment
+                  .start, // Align items to the start of the axis
+              children: <Widget>[
+                // Your form and other widgets here, which will be centered on the screen.
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Mail',
+                      labelStyle: TextStyle(
+                          color: const Color.fromARGB(255, 181, 191,
+                              182)), // Changes the color of the label text
+                      fillColor: Color.fromARGB(255, 247, 247,
+                          246), // Changes the fill color of the text field
+                      filled:
+                          true, // Don't forget to set this to true to activate the fillColor
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(
+                                255, 202, 25, 9)), // Normal border color
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 227, 31,
+                                5)), // Normal border color when the TextFormField is enabled but not focused
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 230, 7,
+                                7)), // Border color when the TextFormField is focused
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Şifre',
+                      labelStyle: TextStyle(
+                          color: Color.fromARGB(255, 149, 160,
+                              150)), // Changes the color of the label text
+                      fillColor: Color.fromARGB(255, 250, 250,
+                          250), // Changes the fill color of the text field
+                      filled:
+                          true, // Don't forget to set this to true to activate the fillColor
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(
+                                255, 207, 61, 8)), // Normal border color
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 212, 46,
+                                13)), // Normal border color when the TextFormField is enabled but not focused
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: const BorderSide(
+                            color: Color.fromARGB(255, 208, 58,
+                                7)), // Border color when the TextFormField is focused
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+
+                ElevatedButton(
+                  child: Text('Giriş Yap'),
+                  onPressed: () {
+                    // Implement login logic, probably with Firebase Auth
+                  },
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    primary: const Color.fromARGB(
+                        255, 229, 226, 226), // This is the text color
+                  ),
+                  child: Text('Hesabınız Yok mu? Üye Ol'),
+                  onPressed: () {
+                    // Navigate to registration page
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'HomeScreen.dart';
-import 'User.dart';
-import 'UserAdress.dart';
-
-import 'package:firebase_database/firebase_database.dart';
+import 'loginPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,32 +10,6 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
-
-  // Example user creation
-  User user = User(
-    name: 'John',
-    surname: 'Doe',
-    email: 'johndoe@example.com',
-    sex: 'Male',
-    age: 30,
-    address: UserAddress(
-      street: '123 Main St',
-      city: 'Anytown',
-      state: 'State',
-      postalCode: '12345',
-    ),
-    userID: 'user123',
-  );
-
-  // Example of adding a pet hash to the user's swipedRight list
-  user.addPetToSwipedRight('petHash123');
-
-  DatabaseReference userRef =
-      FirebaseDatabase.instance.ref().child('users').child(user.userID);
-  userRef.set(user.toJson());
-
-  // Printing to verify the addition
-  print(user.swipedRight);
 }
 
 class MyApp extends StatelessWidget {
@@ -49,7 +20,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: HomeScreen(),
+      //home: HomeScreen(),
+      home: LoginPage(),
     );
   }
 }
